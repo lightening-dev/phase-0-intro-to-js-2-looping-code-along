b8454510dc1d45fb00a0b490ea10db571eb1f41a
Lightening Gibson <lighteningibson@gmail.com>
2022-02-02 14:12:47 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+
+//const name = ["Guadalupe", "Ollie", "Aki"]
+//const eventName = ["surprise"]
+function writeCards(namesArray, eventName) {
+
+    let thankYouCards = [];
+
+    for (let i = 0; i < namesArray.length; i++) { 
+         
+       thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+        
+    }
+
+    return thankYouCards;
+}
+
+ 
+function countDown(callCount) {
+
+
+while (callCount > 0) { 
+    
+    console.log(callCount) 
+    callCount --
+}
+
+console.log (callCount)
+
+}
